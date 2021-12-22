152fa8b5132eccf28cd0136bf618a388d7f9cd6e
arthurlch <lachat.arthur74@gmail.com>
2021-12-22 14:55:24 +0900

    feat: using getter to get each function to delete order relation between each event attributes an sync

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,21 +1,25 @@
 import axios, { AxiosResponse } from "axios";
 import { Eventing } from "./Eventing";
-
+import { Sync } from "./Sync";
+import { Attributes } from "./Attributes";
 export interface UserProps {
   id?: number;
   name?: string;
   age?: number;
 }
+
+// development URL
+const rootUrl = "http://localhost:3000/users";
 export class User {
   public events: Eventing = new Eventing();
+  public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
+  public attributes: Attributes<UserProps>;
 
-  constructor(private data: UserProps) {}
-
-  get(propName: string): number | string {
-    return this.data[propName];
+  constructor(attributes: UserProps) {
+    this.attributes = new Attributes<UserProps>(attributes);
   }
 
-  set(update: UserProps): void {
-    Object.assign(this.data, update);
+  get on() {
+    return this.events.on;
   }
 }
