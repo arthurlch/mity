86b65288fcbd5437da7651ef0193e8158aa4d3f3
arthurlch <lachat.arthur74@gmail.com>
2021-12-23 14:45:15 +0900

    feat: modelattributes for model class

--- src/models/Model.ts
+++ src/models/Model.ts
@@ -1,9 +1,18 @@
 import { Eventing } from './Eventing';
+import { Attributes } from './Attributes';
+import { ApiSync } from './ApiSync';
 
-interface ModelAttributes {}
+interface ModelAttributes<T> {
+  set(value: T): void;
+  getAll(): T;
+  get<K extends keyof T>(key: K): T[K];
+}
 
 interface Sync {}
 
-interface Events {}
+interface Events {
+  on(eventName: string, callback: () => void): void;
+  trigger(eventName: string): void;
+}
 
 export class Model {}
