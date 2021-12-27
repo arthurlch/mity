8ea0807d37be6cb20bdebdf7c564fce8b363c861
arthurlch <lachat.arthur74@gmail.com>
2021-12-27 13:54:12 +0900

    feat: build user collection inside User class

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import { Collection } from './models/Collection';
-
-const collection = new Collection('http://localhost:3000/users');
-
-collection.on('change', () => {
-  console.log(collection);
-});
-collection.fetch();
--- src/models/User.ts
+++ src/models/User.ts
@@ -2,13 +2,14 @@ import { Model } from './Model';
 import { Attributes } from './Attributes';
 import { Eventing } from './Eventing';
 import { ApiSync } from './ApiSync';
+import { Collection } from './Collection';
 export interface UserProps {
   id?: number;
   name?: string;
   age?: number;
 }
 
-// Default development URL
+// By Default the development URL is set to localhost:3000/users
 const rootUrl = 'http://localhost:3000/users';
 export class User extends Model<UserProps> {
   static buildUser(attrs: UserProps) {
@@ -16,6 +17,11 @@ export class User extends Model<UserProps> {
       new Attributes<UserProps>(attrs),
       new Eventing(),
       new ApiSync<UserProps>(rootUrl)
+    );
+  }
+  static buildUserCollection(): Collection<User, UserProps> {
+    return new Collection<User, UserProps>(rootUrl, (json: UserProps) =>
+      User.buildUser(json)
     );
   }
 }
