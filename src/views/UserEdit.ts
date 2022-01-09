eaf2c4b027e204598650d29a6c2090ba829efd64
arthurlch <lachat.arthur74@gmail.com>
2022-01-09 11:17:15 +0900

    test: regionsmaps in userdit

--- src/views/UserEdit.ts
+++ src/views/UserEdit.ts
@@ -1,7 +1,14 @@
 import { View } from './View';
 import { User, UserProps } from '../models/User';
+import { UserShow } from './UserShow';
 
 export class UserEdit extends View<User, UserProps> {
+  regionsMap(): { [key: string]: string } {
+    return {
+      UserShow: '.user-show',
+      UserForm: '.user-form',
+    };
+  }
   template(): string {
     return `
       <div>
